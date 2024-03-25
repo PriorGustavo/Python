07a59858818c3f31d13323911527e9d762bc0bb8
PriorGustavo <159254801+PriorGustavo@users.noreply.github.com>
2024-03-25 15:16:08 -0300

    Add files via upload

---
+++ Exercicio_OP_Art.py
@@ -0,0 +1,15 @@
+import time 
+numero1=float(input("Digite o numero:"))
+numero2=float(input("Digite o outro numero:"))
+
+
+print("Adição:",numero1 + numero2)
+print("Subtração:",numero1 - numero2)
+print("Multiplicação:",numero1 * numero2)
+print("Divisão:",numero1 / numero2)
+print("Divisão inteira:",numero1 // numero2)
+print("Resto da divisão:",numero1 % numero2)
+print("Potencia:",numero1 ** numero2)
+
+
+time.sleep(5)
---
+++ Exercicio_PAR.py
@@ -0,0 +1,12 @@
+import time
+n1 = float(input("Digite o numero:"))
+
+n2 = n1 % 2
+
+if n2 == 0:
+     print('""P-A-R!!!""')
+else:
+     print("Tente outra vez!")
+
+
+time.sleep(5)
